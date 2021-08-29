751e1577f6911eadd846a160ee9f9972e7c5b731
Alm4my <thenixadm@gmail.com>
2021-08-29 01:59:23 +0000

    Implement weather application by city name

---
+++ weather-app/js/index.js
@@ -0,0 +1,82 @@
+const openWeatherAppId = '149becde875ad008414ce575083f4b62';
+const openWeatherUrl = 'http://api.openweathermap.org/data/2.5/weather'
+
+/**
+ * @function prepareData
+ * This function gets the name of the city from the input box, checks if it is valid and
+ * try to get the data. Otherwise it alerts an error message.
+ *
+ * @param units units of the requested weather data either imperial or metric
+*/
+const prepareData = function (units) {
+    let cityName = $('#city-name').val();
+    if (cityName && cityName !== ''){
+        cityName = cityName.trim();
+        getData(openWeatherUrl, cityName, openWeatherAppId, units);
+    }
+    else{
+        alert('Please enter the city name!')
+    }
+}
+
+/**
+ * @function getData
+ * This function gets weather data from the OpenWeather API by sending ajax requests
+ *
+ * @param url API endpoint
+ * @param cityName name of the city we are looking for
+ * @param appId application ID
+ * @param units unit of the result (metric or imperial)
+ */
+function getData(url, cityName, appId, units){
+    $.ajax({
+        url: url,
+        dataType: 'jsonp',
+        data: {
+            q: cityName,
+            appId: appId,
+            units: units,
+        },
+        jsonpCallback: 'fetchData',
+        type: 'GET'
+    }).fail(function (error){
+        console.error(error);
+        alert('Error sending Request')
+    });
+}
+
+/**
+ * @function fetchData
+ * This is the callback of our ajax jsonp request. It gets the data and appends
+ * it to an html variable
+ *
+ * @param forecast
+ */
+function fetchData(forecast){
+    console.log(forecast);
+    let html = '',
+        cityName = forecast.name,
+        country = forecast.sys.country;
+
+    html += `<h3> Weather Forecast for ${cityName}, ${country} </h3>`
+    html += `<em>Temperature: <b>${forecast.main.temp}</b></em>
+                <p>Weather: ${forecast.weather[0].main} <br>
+                   Description: ${forecast.weather[0].description}</p>`
+
+    // for (const entry of forecast.weather)
+    //     html += `<p>Weather: ${entry.main} <br>
+    //                 Description: ${entry.description}</p>`;
+
+
+    $('#log').html(html);
+}
+
+$(document).ready(function (){
+    $('.btn-metric').click(function (){
+        prepareData('metric');
+    })
+
+    $('.btn-imperial').click(function (){
+        prepareData('imperial');
+    })
+})
